83c0d06a997d7310be866a7f43142ae617df59e8
Apinya <159605386+PrangApinya@users.noreply.github.com>
2024-08-04 18:00:22 +0700

    Update Cafe.jsx

--- my_cafe/src/cafe/Cafe.jsx
+++ my_cafe/src/cafe/Cafe.jsx
@@ -7,7 +7,7 @@ const cafe = () => {
             <div class="navigation-items">
                 <a href="#">COFFEE</a>
                 <a href="#">TEA</a>
-                <a href="#">MExzxcvzxvNU</a>
+                <a href="#">MENU</a>
 
             </div>
         </div>
