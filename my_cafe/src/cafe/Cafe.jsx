1ee6ea5111373e78c8ec3267ac4a549026339d8a
Apinya <159605386+PrangApinya@users.noreply.github.com>
2024-08-04 17:48:18 +0700

    main

---
+++ my_cafe/src/cafe/Cafe.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Cafe.css'
+const cafe = () => {
+  return (
+    <header>
+        <div class="navigation">
+            <div class="navigation-items">
+                <a href="#">COFFEE</a>
+                <a href="#">TEA</a>
+                <a href="#">MENU</a>
+
+            </div>
+        </div>
+    </header>
+  )
+
+}
+
+
+export default cafe
---
+++ my_cafe/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Cafe from './cafe/Cafe.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Cafe></Cafe>
+)
