51279d6512bfec3799e7ad07840a477ece93af8b
WatchaphonTee <tynoy2012@gmail.com>
2024-08-08 14:36:29 +0700

    backend0.0.1

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Cafe from './cafe/Cafe.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Cafe/>
+  <App/>
 )
